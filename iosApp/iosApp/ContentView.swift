cdbb67bc8e1aea2bcf9ee341dd1a25e59cc7f31e
Anthony Hofmeister <anthonyhofmeister@gmx.de>
2025-02-15 16:27:57 +0100

    iOS App Icon

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
     var body: some View {
         ComposeView()
             .ignoresSafeArea(.all)
-                .ignoresSafeArea(.keyboard) // Compose has own keyboard handler
+            .ignoresSafeArea(.keyboard) // Compose has own keyboard handler
     }
 }
 
