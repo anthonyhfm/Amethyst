1ffd78476a85654f6b88f29e485a37dd2f60bc0e
Anthony Hofmeister <anthonyhofmeister@gmx.de>
2025-02-16 16:10:30 +0100

    Handle MIDI Input on iOS

--- composeApp/src/iosMain/kotlin/dev/anthonyhfm/amethyst/core/midi/data/MidiInputData.ios.kt
+++ composeApp/src/iosMain/kotlin/dev/anthonyhfm/amethyst/core/midi/data/MidiInputData.ios.kt
@@ -1,5 +1,12 @@
 package dev.anthonyhfm.amethyst.core.midi.data
 
 actual fun getMidiInputData(byteArray: ByteArray): MidiInputData? {
-    TODO("Not yet implemented")
+    if (byteArray.size == 3 && byteArray[0] in 144.toByte() .. 159.toByte()) {
+        return MidiInputData(
+            pitch = DRUM_RACK_TO_XY[byteArray[1].toInt()],
+            velocity = byteArray[2].toInt()
+        )
+    }
+
+    return null
 }
