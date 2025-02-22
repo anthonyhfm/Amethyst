8d4bd58f08fe1b40589e9ca189e047474ed3df1c
Anthony Hofmeister <anthonyhofmeister@gmx.de>
2025-02-22 21:24:38 +0100

    Viewport MF64 and LPPROMK3

---
+++ composeApp/src/commonMain/kotlin/dev/anthonyhfm/amethyst/ui/launchpad/viewport_launchpads/ViewportMidiFighter64.kt
@@ -0,0 +1,105 @@
+package dev.anthonyhfm.amethyst.ui.launchpad.viewport_launchpads
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.dp
+import dev.anthonyhfm.amethyst.core.midi.data.MidiEffectData
+import dev.anthonyhfm.amethyst.ui.launchpad.components.GenericLaunchpadButton
+import dev.anthonyhfm.amethyst.ui.launchpad.components.GenericLaunchpadLayout
+import dev.anthonyhfm.amethyst.ui.launchpad.components.LaunchpadLayout
+import dev.anthonyhfm.amethyst.workspace.ui.viewport.elements.LaunchpadViewportElement
+
+class ViewportMidiFighter64(
+    override var shape: Shape = RoundedCornerShape(8),
+    override var size: Size = Size(8f, 8f),
+) : LaunchpadViewportElement() {
+    override val content: @Composable (() -> Unit) = {
+        val previewGrid by previewState.grid.collectAsState()
+
+        GenericLaunchpadLayout(
+            layoutType = LaunchpadLayout.LAYOUT_8X8,
+            modifier = Modifier
+                .size(width = size.width.dp * 40, height = size.height.dp * 40)
+                .clip(shape)
+                .background(Color(0xFF0d0d0d))
+                .padding(8.dp),
+        ) { x, y ->
+            GridPad(
+                x = x,
+                y = y,
+                effectData = previewGrid[x][y],
+                onClick = null
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+private fun GridPad(
+    x: Int,
+    y: Int,
+    effectData: MidiEffectData,
+    onClick: (() -> Unit)?,
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .then(
+                if (onClick != null) {
+                    Modifier.combinedClickable(
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = null,
+                        onClick = {
+                            onClick()
+                        }
+                    )
+                } else {
+                    Modifier
+                }
+            ),
+
+        contentAlignment = Alignment.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize(),
+
+            contentAlignment = Alignment.Center
+        ) {
+            GenericLaunchpadButton(
+                sizeModifier = Modifier
+                    .fillMaxSize(0.8f),
+                effect = effectData,
+                enableLightSpot = false,
+                shape = CircleShape
+            )
+
+            Box(
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .fillMaxSize(0.62f)
+                    .background(Color(0xFF0A0A0A))
+            )
+        }
+    }
+}
