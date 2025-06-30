d9aa602917b4db43365b18e84b6e79a0168c5682
Anthony Hofmeister <anthonyhofmeister@gmx.de>
2025-06-30 20:26:49 +0200

    Compose Hot Reload for development on Desktop

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
     kotlin("plugin.serialization") version "2.1.0"
+    id("org.jetbrains.compose.hot-reload") version "1.0.0-beta02"
 }
 
 kotlin {
