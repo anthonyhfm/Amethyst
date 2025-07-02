f949193a6215f9138455535aff9e558e28ff58f7
Anthony Hofmeister <anthonyhofmeister@gmx.de>
2025-07-02 22:40:32 +0200

    Some Menu Bar functionality

--- composeApp/src/desktopMain/kotlin/dev/anthonyhfm/amethyst/workspace/ui/WorkspaceMenuBar.kt
+++ composeApp/src/desktopMain/kotlin/dev/anthonyhfm/amethyst/workspace/ui/WorkspaceMenuBar.kt
@@ -1,9 +1,11 @@
 package dev.anthonyhfm.amethyst.workspace.ui
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.window.FrameWindowScope
 import androidx.compose.ui.window.MenuBar
 import androidx.lifecycle.viewmodel.compose.viewModel
+import dev.anthonyhfm.amethyst.workspace.WorkspaceContract
 
 @Composable
 fun FrameWindowScope.WorkspaceMenuBar() {
@@ -13,19 +15,61 @@ fun FrameWindowScope.WorkspaceMenuBar() {
         Menu(
             text = "File"
         ) {
+            Item(
+                text = "Open Project",
+                onClick = {
+                    viewModel.openProject()
+                }
+            )
 
-        }
+            Separator()
 
-        Menu(
-            text = "Edit"
-        ) {
+            Item(
+                text = "Save",
+                onClick = {
+                    viewModel.saveProject()
+                }
+            )
 
+            Item(
+                text = "Save As..",
+                onClick = {
+                    viewModel.saveProjectAs()
+                }
+            )
+
+            Separator()
+
+            Item(
+                text = "Close Project",
+                onClick = {
+                    viewModel.closeProject()
+                }
+            )
         }
 
         Menu(
             text = "View"
         ) {
+            Menu(
+                text = "Workspace Mode"
+            ) {
+                Item("Layout") {
+                    viewModel.switchMode(WorkspaceContract.WorkspaceMode.Layout())
+                }
 
+                Item("Preview") {
+                    viewModel.switchMode(WorkspaceContract.WorkspaceMode.Preview())
+                }
+
+                Item("Lights (Chain-Editor)") {
+                    viewModel.switchMode(WorkspaceContract.WorkspaceMode.LightsChain())
+                }
+
+                Item("Sampling (Chain-Editor)") {
+                    viewModel.switchMode(WorkspaceContract.WorkspaceMode.SamplingChain())
+                }
+            }
         }
     }
 }
--- composeApp/src/desktopMain/kotlin/dev/anthonyhfm/amethyst/workspace/ui/WorkspaceMenuBarViewModel.kt
+++ composeApp/src/desktopMain/kotlin/dev/anthonyhfm/amethyst/workspace/ui/WorkspaceMenuBarViewModel.kt
@@ -1,7 +1,27 @@
 package dev.anthonyhfm.amethyst.workspace.ui
 
 import androidx.lifecycle.ViewModel
+import dev.anthonyhfm.amethyst.workspace.WorkspaceContract
+import dev.anthonyhfm.amethyst.workspace.WorkspaceRepository
 
 class WorkspaceMenuBarViewModel : ViewModel() {
+    fun openProject() {
+        // Logic to open a project
+    }
 
+    fun saveProject() {
+        // Logic to save the current project
+    }
+
+    fun saveProjectAs() {
+        // Logic to save the current project as a new file
+    }
+
+    fun closeProject() {
+        // Logic to close the current project
+    }
+
+    fun switchMode(mode: WorkspaceContract.WorkspaceMode) {
+        WorkspaceRepository.switchMode(mode)
+    }
 }
