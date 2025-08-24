72ab2421df6245a6d28cb228331a417610d92b7c
Anthony Hofmeister <anthonyhofmeister@gmx.de>
2025-08-24 11:56:21 +0200

    Echo - Audio Engine (tbd)

---
+++ composeApp/src/commonMain/kotlin/dev/anthonyhfm/amethyst/core/echo/Echo.kt
@@ -0,0 +1,12 @@
+package dev.anthonyhfm.amethyst.core.echo
+
+/**
+ * # Echo
+ *
+ * Echo is the Audio Engine of Amethyst, providing low-latency audio processing and effects.
+ * It is designed to handle real-time audio manipulation, ensuring high performance and
+ * minimal latency.
+ */
+object Echo {
+
+}
