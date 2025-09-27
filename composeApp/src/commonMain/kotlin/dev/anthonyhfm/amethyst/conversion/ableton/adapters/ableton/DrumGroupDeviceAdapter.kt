e5095ac728fb0607c65c93bf9708ae3c428df86e
trobonox <57040351+trobonox@users.noreply.github.com>
2025-09-27 18:21:32 +0200

    Implement support for samples multisampling

--- composeApp/src/commonMain/kotlin/dev/anthonyhfm/amethyst/conversion/ableton/adapters/ableton/DrumGroupDeviceAdapter.kt
+++ composeApp/src/commonMain/kotlin/dev/anthonyhfm/amethyst/conversion/ableton/adapters/ableton/DrumGroupDeviceAdapter.kt
@@ -1,6 +1,8 @@
 package dev.anthonyhfm.amethyst.conversion.ableton.adapters.ableton
 
 import dev.anthonyhfm.amethyst.conversion.ableton.adapters.AbletonAdapter
+import dev.anthonyhfm.amethyst.conversion.ableton.adapters.ableton.MxDeviceMidiEffectAdapter.Companion.readDataBlob
+import dev.anthonyhfm.amethyst.conversion.ableton.adapters.outbreak.MultiAdapter
 import dev.anthonyhfm.amethyst.conversion.ableton.utils.XmlElement
 import dev.anthonyhfm.amethyst.core.midi.data.DRUM_RACK_TO_XY
 import dev.anthonyhfm.amethyst.devices.DeviceState
@@ -37,6 +39,55 @@ class DrumGroupDeviceAdapter(
                                         )
                                     )
                                 )
+
+                                // Multisampling logic
+                                // TODO: replace simple multi name checking for max plugin with hash check
+                                val branchElements = branch.querySelector("DeviceChain")[0]
+                                    .querySelector("Devices")[0]
+                                    .children
+
+                                if (branchElements.size >= 2) {
+                                    val multiDevice = branchElements.find {
+                                        (it.querySelector("Name")[0].attributes["Value"]?.lowercase()?.contains("multi")
+                                            ?: false)
+                                        && !it.querySelector("Name")[0].attributes["Value"]?.lowercase()
+                                            ?.contains("reset")!!
+                                    }
+                                    val randomDevice = branchElements.find {
+                                        it.name == "MidiRandom"
+                                    }
+                                    val samplesContainer = branchElements.find {
+                                        it.name == "InstrumentGroupDevice"
+                                                || it.name == "DrumGroupDevice"
+                                    }
+
+                                    if (multiDevice != null && samplesContainer != null) {
+                                        println("Found multi and container, using MultiAdapter")
+                                        val multiDataBlob = multiDevice.localQuerySelector("BlobSlot")[0]
+                                            .localQuerySelector("Value")[0]
+                                            .localQuerySelector("MxDBlob")[0]
+                                            .localQuerySelector("Blob")[0]
+
+                                        addAll(
+                                            MultiAdapter(
+                                                blob = readDataBlob(multiDataBlob.text!!),
+                                                containerXml = samplesContainer
+                                            ).toDeviceStates()
+                                        )
+
+                                        return@apply
+                                    } else if (randomDevice != null && samplesContainer != null) {
+                                        println("Found random and container, using RandomDeviceMultisamplingAdapter")
+                                        addAll(
+                                            RandomDeviceMultisamplingAdapter(
+                                                randomDeviceXml = randomDevice,
+                                                containerXml = samplesContainer
+                                            ).toDeviceStates()
+                                        )
+
+                                        return@apply
+                                    }
+                                }
 
                                 addAll(
                                     branch.querySelector("DeviceChain")[0]
