6b6c9001fbd651f90e16602623554730cb273833
Anthony Hofmeister <anthonyhofmeister@gmx.de>
2025-10-30 17:59:40 +0100

    iOS Byte-Hash

--- composeApp/src/iosMain/kotlin/dev/anthonyhfm/amethyst/conversion/ableton/utils/PlatformFileHash.ios.kt
+++ composeApp/src/iosMain/kotlin/dev/anthonyhfm/amethyst/conversion/ableton/utils/PlatformFileHash.ios.kt
@@ -2,6 +2,7 @@ package dev.anthonyhfm.amethyst.conversion.ableton.utils
 
 import io.github.vinceglb.filekit.PlatformFile
 import io.github.vinceglb.filekit.path
+import io.github.vinceglb.filekit.utils.toNSData
 import kotlinx.cinterop.*
 import platform.CoreCrypto.CC_MD5
 import platform.CoreCrypto.CC_MD5_DIGEST_LENGTH
@@ -13,6 +14,21 @@ actual fun PlatformFile.getFileHash(): String {
     val nsString = NSString.stringWithString(this.path)
     val fileData = NSData.dataWithContentsOfFile(nsString)
         ?: return ""
+
+    return memScoped {
+        val digest = allocArray<UByteVar>(CC_MD5_DIGEST_LENGTH)
+
+        CC_MD5(fileData.bytes, fileData.length.toUInt(), digest)
+
+        (0 until CC_MD5_DIGEST_LENGTH).joinToString("") {
+            digest[it].toString(16).padStart(2, '0')
+        }
+    }
+}
+
+@OptIn(ExperimentalForeignApi::class)
+actual fun ByteArray.toFileHash(): String {
+    val fileData = toNSData()
 
     return memScoped {
         val digest = allocArray<UByteVar>(CC_MD5_DIGEST_LENGTH)
