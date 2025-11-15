6a0d1f60a441831661bc9b0fdb11827a43211c92
Anthony Hofmeister <anthony.hofmeister@bonprix.net>
2025-11-15 19:55:46 +0100

    Add empty surface

--- composeApp/src/commonMain/kotlin/dev/anthonyhfm/amethyst/timeline/PianoRollWorkspaceMode.kt
+++ composeApp/src/commonMain/kotlin/dev/anthonyhfm/amethyst/timeline/PianoRollWorkspaceMode.kt
@@ -1,7 +1,12 @@
 package dev.anthonyhfm.amethyst.timeline
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.key.KeyEvent
 import dev.anthonyhfm.amethyst.core.engine.heaven.Heaven
 import dev.anthonyhfm.amethyst.timeline.data.MidiEntry
@@ -20,6 +25,14 @@ class PianoRollWorkspaceMode : WorkspaceContract.WorkspaceMode {
     fun ModeContent(paddingValues: PaddingValues) {
         val entry = currentEntry ?: return
         val launchpads = Heaven.devices
+
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.surface)
+                .padding(paddingValues)
+        ) {
+        }
     }
 
     override fun onKeyEvent(event: KeyEvent): Boolean {
