23c2080c81804aba9a34420a5f3d0c0ca45e522c
Anthony Hofmeister <anthonyhofmeister@gmx.de>
2025-11-28 23:57:11 +0100

    Deselect when click beside a device

--- composeApp/src/commonMain/kotlin/dev/anthonyhfm/amethyst/workspace/chain/ui/WorkspaceChainEditor.kt
+++ composeApp/src/commonMain/kotlin/dev/anthonyhfm/amethyst/workspace/chain/ui/WorkspaceChainEditor.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -79,7 +80,14 @@ fun WorkspaceChainEditor(
 
     Column(
         modifier = Modifier
-            .padding(12.dp),
+            .padding(12.dp)
+            .clickable(
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() },
+                onClick = {
+                    SelectionManager.clear()
+                }
+            ),
 
         verticalArrangement = Arrangement.spacedBy(6.dp)
     ) {
