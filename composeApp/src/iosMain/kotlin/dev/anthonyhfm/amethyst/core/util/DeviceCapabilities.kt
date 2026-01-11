cb32af0b9e8a5c3b5a3643719b9ee1cdd3695c4b
Anthony Hofmeister <anthonyhofmeister@gmx.de>
2026-01-12 00:05:31 +0100

    Device capabilities

--- composeApp/src/iosMain/kotlin/dev/anthonyhfm/amethyst/core/util/DeviceCapabilities.kt
+++ composeApp/src/iosMain/kotlin/dev/anthonyhfm/amethyst/core/util/DeviceCapabilities.kt
@@ -23,11 +23,18 @@ actual fun getDeviceCapabilities(): DeviceCapabilities {
         else -> true
     }
 
+    val defaultFPS = when {
+        (iphoneGen ?: Int.MAX_VALUE) >= 13 -> 90
+        isIpad -> 120
+
+        else -> 60
+    }
+
     val lowRam = (isIphone && (iphoneGen ?: Int.MAX_VALUE) < 13) || (isIpad && !isIpadFpsAllowed(modelIdentifier))
 
     return DeviceCapabilities(
         showFPSSettings = showFps,
-        initialFPS = 60,
+        initialFPS = defaultFPS,
         showGradientSmoothnessSettings = true,
         lowRamUsageMode = lowRam
     )
@@ -61,28 +68,5 @@ private fun parseIphoneModel(identifier: String): Pair<Boolean, Int?> {
 }
 
 private fun isIpadFpsAllowed(identifier: String): Boolean {
-    if (!identifier.startsWith("iPad")) return false
-
-    // iPad Pro (2. Gen, 2017): iPad7,1–iPad7,4
-    val isIpadPro2 = Regex("^iPad7,[1-4]$").matches(identifier)
-
-    // iPad Pro (2018/2020): iPad8,1–iPad8,12
-    val isIpadPro3Or4 = Regex("^iPad8,([1-9]|1[0-2])$").matches(identifier)
-
-    // iPad Pro (2021, M1): iPad13,4–iPad13,11
-    val isIpadPro5 = Regex("^iPad13,([4-9]|1[0-1])$").matches(identifier)
-
-    // iPad Pro (2022, M2): iPad14,3–iPad14,6
-    val isIpadPro6 = Regex("^iPad14,[3-6]$").matches(identifier)
-
-    // iPad Air (4. Gen, 2020): iPad13,1–iPad13,2
-    val isIpadAir4 = Regex("^iPad13,[1-2]$").matches(identifier)
-
-    // iPad Air (5. Gen, 2022): iPad13,16–iPad13,17
-    val isIpadAir5 = Regex("^iPad13,1[6-7]$").matches(identifier)
-
-    // iPad Air (6. Gen, 2024): iPad14,8–iPad14,9
-    val isIpadAir6 = Regex("^iPad14,[8-9]$").matches(identifier)
-
-    return isIpadPro2 || isIpadPro3Or4 || isIpadPro5 || isIpadPro6 || isIpadAir4 || isIpadAir5 || isIpadAir6
+    return identifier.startsWith("iPad")
 }
