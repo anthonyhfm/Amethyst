6d918e8b6892ed3ac12ae99582a225e3bade098b
Anthony Hofmeister <anthonyhofmeister@gmx.de>
2026-09-04 18:37:20 +0200

    Bump to 0.7.0

--- composeApp/src/commonMain/kotlin/dev/anthonyhfm/amethyst/core/util/Version.kt
+++ composeApp/src/commonMain/kotlin/dev/anthonyhfm/amethyst/core/util/Version.kt
@@ -9,7 +9,7 @@ data class Version(
     val hotfix: Int
 )
 
-val amethystVersion = Version(0, 6, 4)
+val amethystVersion = Version(0, 7, 0)
 
 val Version.displayString: String
     get() = "$major.$minor.$hotfix"
